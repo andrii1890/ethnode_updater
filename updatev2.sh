d39d90aa1feec6581224835ed66d366ff2cf680d
st.Game | acloud.pp.ua <95629373+andrii1890@users.noreply.github.com>
2023-04-10 14:15:13 +0300

    Update updatev2.sh

--- updatev2.sh
+++ updatev2.sh
@@ -1,14 +1,23 @@
+#!/bin/bash
+#LATEST_RELEASE_ERIGON=LRE
+#LATEST_VERSION_ERIGON=LVE
+#LATEST_RELEASE_LIGHTHOUSE=LRL
+#LATEST_VERSION_LIGHTHOUSE=LVL
 GREEN_COLOR='\033[0;32m'
 RED_COLOR='\033[0;31m'
 NO_COLOR='\033[0m'
-ERIGON=v2.42.0
-LIGHTHOUSE=v4.0.1
+LRE=$(curl -L -s -H 'Accept: application/json' https://github.com/ledgerwatch/erigon/releases/latest)
+LVE=$(echo $LRE | sed -e 's/.*"tag_name":"\([^"]*\)".*/\1/')
+LRL=$(curl -L -s -H 'Accept: application/json' https://github.com/sigp/lighthouse/releases/latest)
+LVL=$(echo $LRL | sed -e 's/.*"tag_name":"\([^"]*\)".*/\1/')
+ERIGON=erigon-$LVE
+LIGHTHOUSE=lighthouse-$LVL
 
          echo -e "$GREEN_COLOR Script will update ERIGON and LIGHTHOUSE to version: $ERIGON and $LIGHTHOUSE $NO_COLOR\n"
-         echo -e "$GREEN_COLOR Stop services... $NO_COLOR\n"
+         echo -e "$GREEN_COLOR Stoping services... $NO_COLOR\n"
          sudo systemctl stop erigon.service lighthouse.service
 
-              for (( timer=120; timer>0; timer-- )); do
+              for (( timer=60; timer>0; timer-- )); do
                      printf "* waiting for services stopped correctly ${RED_COLOR}%02d${NO_COLOR} sec\r" $timer
                      sleep 1
               done
@@ -21,23 +30,21 @@ LIGHTHOUSE=v4.0.1
          echo -e "$GREEN_COLOR Delete previous erigon and lighthouse folder $NO_COLOR\n"
          cd $HOME && rm -rf erigon/ lighthouse/
          
-         echo -e "$GREEN_COLOR Clone erigon repo...build binaries...move to working folder $ERIGON... $NO_COLOR\n"
+         echo -e "$GREEN_COLOR Clone erigon repo > build binaries > move to working folder $ERIGON $NO_COLOR\n"
          git clone https://github.com/ledgerwatch/erigon.git
          cd erigon
-         git checkout $ERIGON
+         git checkout $LVE
          make erigon && cd build/bin/ && sudo cp erigon /usr/local/bin/
         
         
-         echo -e "$GREEN_COLOR Clone Lighthouse repo...build binaries...move to working folder $LIGHTHOUSE... $NO_COLOR\n"
+         echo -e "$GREEN_COLOR Clone Lighthouse repo > build binaries > move to working folder $LIGHTHOUSE $NO_COLOR\n"
          cd $HOME/
          git clone https://github.com/sigp/lighthouse.git
-         latestTag=$(curl -s https://api.github.com/repos/sigp/lighthouse/releases/latest | grep '.tag_name'|cut -d\" -f4)
-         echo $latestTag
          cd lighthouse
-         git checkout $latestTag
+         git checkout $LVL
          make && cd $HOME/.cargo/bin/ && sudo cp lighthouse /usr/local/bin/
          
-         echo -e "$GREEN_COLOR Restart services... $NO_COLOR\n"
+         echo -e "$GREEN_COLOR Restarting services... $NO_COLOR\n"
          sudo systemctl restart erigon.service lighthouse.service
          echo -e "$GREEN_COLOR Check ERIGON and LIGHTHOUSE version..."
          erigon -v && lighthouse -V
